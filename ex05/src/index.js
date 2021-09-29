a30b948734bcf4022d4dc0786046d7fef900f5b6
BOZO <bozoperi@gmail.com>
2021-09-29 23:11:33 +0200

    5 ex added

---
+++ ex05/src/index.js
@@ -0,0 +1,13 @@
+// Only change code bellow this line
+var dress = "little dress";
+
+function myDress() {
+    var dress = "ball gown";
+    return dress;   
+
+}
+dress=myDress();
+// Only change code above this line
+console.log(dress);
+
+module.exports=myDress;
